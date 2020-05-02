577d946b10f8a74cd8fcd9da7106c24c76c1dfa8
MichalLuzyna <michal.luzyna@capgemini.com>
2020-05-02 15:21:56 +0200

    console

--- assignment5/js/script.js
+++ assignment5/js/script.js
@@ -102,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories);
-      Console.log(chooseRandomCategory);
+      console.log(chooseRandomCategory);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
